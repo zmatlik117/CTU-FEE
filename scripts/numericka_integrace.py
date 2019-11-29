2b6da5fb04d41dd861362707990defa6c6ffa5c1
Jan Zmatlik <zmatlik.117@gmail.com>
2019-11-29 22:31:37 +0100

    Drobne opravy
    Signed-off-by: Jan Zmatlik <zmatlik.117@gmail.com>

--- scripts/numericka_integrace.py
+++ scripts/numericka_integrace.py
@@ -10,19 +10,20 @@ step = int(1e6)
 low_np = 0
 low_sp = 0
 
-high_np = np.pi + 1         # todle je cislo spocitane na docela dost desetinnych mist
-high_sp = sympy.pi + 1      # ale todle uz je matematicky presny symbol!!
+high_np = np.pi + 1        # todle je cislo spocitane na docela dost desetinnych mist
+high_sp = sympy.pi + 1     # ale todle uz je matematicky presny symbol!!
 
 x_axis = np.linspace(low_np, high_np, step)
 # vycislim funkci
 vals = np.sin(x_axis)
 # pouziju simpsona
 result = integrate.simps(vals, x_axis)
-# a vysypu to do konzole - v tomhle pripade tak nejak cekam ze kdyz zintegruju horni pulku
-# sinusovky ze to vyhodi 2
-print(result)
+# a vysypu to do konzole
+print("numerical solution: %s" % result)
 
 # matematicky presny vysledek. cerna magie. Prostudujte si dokumentaci k modulu sympy
+# hodi se i jupyter-qtconsole + QTcko + latex, pak to dela hezkej vypis
+# ale je hromada patlani s instalaci
 i, x = sympy.symbols('i, x')
 i = sympy.Integral(sympy.sin(x), (x, low_sp, high_sp))
 print("Integral to solve: %s" % i)
