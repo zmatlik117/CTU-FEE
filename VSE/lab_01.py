aeee87d592ee24cf5b45f45506d10e3a70f90839
Jan Zmatlik <zmatlik.117@gmail.com>
2020-05-31 16:27:08 +0200

    Vyresena lab_01
    Signed-off-by: Jan Zmatlik <zmatlik.117@gmail.com>

--- VSE/lab_01.py
+++ VSE/lab_01.py
@@ -1,5 +1,17 @@
+# -*- coding: UTF-8 -*-
 """
 Teplotni zavislost tranzistoru
+
+Z casovych duvodu byla merena pouze teplotni zavislost IGBT tranzistoru.
+
+Z grafu je patrne, ze s rostouci teplotou pri Ugs=const Id prudce roste,
+v zavislosti na pracovnim bode i vice nez dvojnasobne!
+V pripade ze by nekdy nekdo hodlal IGBT tranzistor pouzit jinak nez jako spinace,
+t.j. v linearnim rezimu, musi pri navrhu obvodu toto nutne vzit v uvahu.
+
+V pripade uziti paralelnich IGBT pro zvyseni max Id je nutno mit je na chladici
+co nejbliz k sobe, idealne na stejnem cipu - teploti zavislost by snadno mohla
+zpusobit pretizeni nektereho z nich a pak skonci v kremikovem nebi vsechny.
 """
 
 from matplotlib import pyplot as plt
@@ -11,9 +23,7 @@ transistor = {
          [0, 0, 0.05, 0.25, 0.6, 0.9, 1.3, 2]),
     75: ([0, 4, 4.5, 4.75, 4.95, 5.15, 5.25, 5.35],
          [0, 0, 0.1, 0.2, 0.5, 1.1, 1.65, 2.4])
-
 }
-
 
 plt.xlabel("Ugs[V]")
 plt.ylabel("Id[A]")
@@ -21,7 +31,8 @@ plt.title("IRG4PH40U")
 for temp, char in transistor.items():
     x = char[0]
     y = char[1]
-    plt.plot(x,y,label=str(temp)+"\u00b0C")
+    plt.plot(x, y, label=str(temp)+"°C", marker='s')
+    plt.xlim(3, 6)
     plt.grid()
     plt.legend()
 plt.show()
