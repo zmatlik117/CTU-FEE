6d873a580d3f3b9b266ba929b4397128aacffbb2
Jan Zmatlik <zmatlik.117@gmail.com>
2020-06-23 01:31:17 +0200

    Final verze spinani indukcnosti
    Signed-off-by: Jan Zmatlik <zmatlik.117@gmail.com>

--- semestr_02/VSE/lab_03.py
+++ semestr_02/VSE/lab_03.py
@@ -7,73 +7,52 @@ Fazova trajektorie spinani ruznych typu zateze tranzistorem - myslim ze to byl K
 from matplotlib import pyplot as plt
 import numpy as np
 
-POINTS = 100
+POINTS = 500
 
-def graf_time(t, u, i, dt, dv, di):
+def graf_time(t, u, i, dt, dv, di, title):
     t_ax = [time * dt for time in t]
     i_ax = [curr * di for curr in i]
     v_ax = [volt * dv for volt in u]
-    fig = plt.figure(dpi=200)
+    fig = plt.figure()
     ax = fig.add_subplot()
-    ax.set_xlabel("t[uS]")
+    ax.set_xlabel("t[S]")
     ax.set_ylabel("")
     ax.plot(t_ax, i_ax, label='Ic[A]')
     ax.plot(t_ax, v_ax, label='Uce[V]')
     ax.grid()
     ax.legend(loc='upper left')
-    plt.show()
-
-def graf_xy(u, i, dv, di):
-    i_ax = [curr * di for curr in i]
-    v_ax = [volt * dv for volt in u]
-    fig = plt.figure(dpi=200)
-    ax = fig.add_subplot()
-    ax.set_xlabel("Uce[V]")
-    ax.set_ylabel("Ic[A]")
-    ax.plot(v_ax, i_ax)
-    ax.grid()
+    plt.title(title)
     plt.show()
 
 
-def graf_xy_soar(u, i, dv, di, ic, uce, pmax, p_u, u_p):
+def graf_xy_soar(u, i, dv, di, ic, uce, pmax, p_u, u_p, title):
     i_ax = [curr * di for curr in i]
     v_ax = [volt * dv for volt in u]
     # soar
-    i_max_ax = np.full((POINTS + 1, 1), ic)
-    u_ax = np.linspace(0, uce, POINTS + 1)
-    soar = np.zeros([POINTS + 1])
+    i_max_ax = np.full((POINTS, 1), ic)
+    u_ax = np.linspace(0, uce, POINTS)
+    soar = np.zeros([POINTS])
+    soar_reduced = np.zeros([POINTS])
     i_pm = pmax / u_ax
-    for idx in range(0, POINTS + 1):
+    i_pm_reduced = p_u / u_ax
+    for idx in range(0, POINTS):
         if i_pm[idx] > i_max_ax[idx]:
             soar[idx] = i_max_ax[idx]
+            soar_reduced[idx] = i_max_ax[idx]
         else:
             soar[idx] = i_pm[idx]
+            if idx * uce < POINTS * u_p:
+                soar_reduced[idx] = i_pm[idx]
+            else:
+                soar_reduced[idx] = i_pm_reduced[idx]
     fig = plt.figure()
     ax = fig.add_subplot()
     ax.set_xlabel("Uce[V]")
     ax.set_ylabel("Ic[A]")
-    ax.plot(v_ax, i_ax, label='KD503 op area')
-    ax.plot(u_ax, soar, label='KD503 SOAR')
+    ax.plot(v_ax, i_ax, label=title)
+    ax.plot(u_ax, soar, label='SOAR')
+    ax.plot(u_ax, soar_reduced, label='SOAR@T=100°C')
     ax.legend()
-    ax.grid()
-    plt.show()
-
-
-def graf_SOAR(ic, uce, pmax, p_u, u_p):
-    i_max_ax = np.full((POINTS + 1, 1), ic)
-    u_ax = np.linspace(0, uce, POINTS + 1)
-    soar = np.zeros([POINTS + 1])
-    i_pm = pmax / u_ax
-    for idx in range(0, POINTS + 1):
-        if i_pm[idx] > i_max_ax[idx]:
-            soar[idx] = i_max_ax[idx]
-        else:
-            soar[idx] = i_pm[idx]
-    fig = plt.figure()
-    ax = fig.add_subplot()
-    ax.set_xlabel("Uce[V]")
-    ax.set_ylabel("Ic[A]")
-    ax.plot(u_ax, soar)
     ax.grid()
     plt.show()
 
@@ -82,17 +61,50 @@ def graf_SOAR(ic, uce, pmax, p_u, u_p):
 ic_max = 20  # A
 uce_max = 80  # V
 p_max = 150  # W
-p_u = 70  # W
+p_u = 65  # W
 u_p = 30  # V
 
 # resistive load
-t_div = 200E-6
-v_div = 50  # V/div
-i_div = 1  # 1A/1V
+title = 'R zatez'
+r_t_div = 200E-6
+r_v_div = 50  # V/div
+r_i_div = 1  # 1A/1V
 r_t = [0.1, 0.2, 0.2, 1.0, 3.0, 4.0, 5.0, 6.0, 7.0, 7.4, 7.4, 7.5]
 r_u = [0.3, 0.3, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.3, 0.3]
 r_i = [0.1, 0.1, 0.1, 0.4, 1.3, 1.75, 2.0, 2.33, 2.66, 2.7, 0.1, 0.1]
 
-graf_xy_soar(r_u, r_i, v_div, i_div, ic_max, uce_max, p_max, p_u, u_p)
+graf_time(r_t, r_u, r_i, r_t_div, r_v_div, r_i_div, title)
+graf_xy_soar(
+    r_u, r_i, r_v_div, r_i_div,
+    ic_max, uce_max, p_max, p_u, u_p,
+    title)
 
+# RL load
+title = 'RL bez kompenzace'
+rl_t_div = 200E-6
+rl_v_div = 50  # V/div
+rl_i_div = 1  # 1A/1V
+rl_t = [0.1, 0.2, 0.2, 1.0, 3.0, 5.0, 7.0, 7.4, 7.5, 7.6, 7.7, 7.8]
+rl_u = [0.3, 0.3, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 2.4, 2.2, 0.5, 0.3]
+rl_i = [0.1, 0.1, 0.1, 0.33, 1.33, 1.75, 2.8, 3, 1.5, 0.1, 0.1, 0.1]
 
+graf_time(rl_t, rl_u, rl_i, rl_t_div, rl_v_div, rl_i_div, title)
+graf_xy_soar(
+    rl_u, rl_i, rl_v_div, rl_i_div,
+    ic_max, uce_max, p_max, p_u, u_p,
+    title)
+
+# RL load with diode
+title = 'RL + dioda'
+rld_t_div = 200E-6
+rld_v_div = 50  # V/div
+rld_i_div = 1  # 1A/1V
+rld_t = [0.1, 0.2, 0.2, 1.0, 3.0, 5.0, 7.0, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8.0, 8.2]
+rld_u = [0.3, 0.3, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 1, 0.8, 0.66, 0.5, 0.45, 0.4, 0.3]
+rld_i = [0.1, 0.1, 0.1, 0.33, 1.33, 1.75, 2.8, 3, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+
+graf_time(rld_t, rld_u, rld_i, rld_t_div, rld_v_div, rld_i_div, title)
+graf_xy_soar(
+    rld_u, rld_i, rld_v_div, rld_i_div,
+    ic_max, uce_max, p_max, p_u, u_p,
+    title)
